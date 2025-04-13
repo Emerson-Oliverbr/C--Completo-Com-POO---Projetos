97d4fbb31df716fc5599a114a94c6c7263bca020
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-13 17:42:22 -0300

    Iniciando Modelulo sobre Comportamento de memoria, Arrays e Listas

---
+++ Comportamento de memoria, Arrays, Listas/TiposReferenciaETiposValor/TiposReferenciaETiposValor/Point.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TiposReferenciaETiposValor
+{
+    internal struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Comportamento de memoria, Arrays, Listas/TiposReferenciaETiposValor/TiposReferenciaETiposValor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TiposReferenciaETiposValor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            p = new Point();
+
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
+
