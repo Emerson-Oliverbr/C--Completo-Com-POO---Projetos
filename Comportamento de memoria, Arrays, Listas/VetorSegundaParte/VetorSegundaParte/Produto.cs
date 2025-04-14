63009313e46cb829e70be4a1d64951275e9cd967
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-14 20:12:30 -0300

    Vetores parte 2

---
+++ Comportamento de memoria, Arrays, Listas/VetorSegundaParte/VetorSegundaParte/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace VetorSegundaParte
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Comportamento de memoria, Arrays, Listas/VetorSegundaParte/VetorSegundaParte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+using VetorSegundaParte;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for(int i=0; i<n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double somaDosPrecos = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                somaDosPrecos += vect[i].Preco;
+            }
+
+            double media = somaDosPrecos / n;
+
+            Console.WriteLine($"Media dos preços = R${media.ToString("F2",CultureInfo.InvariantCulture)} ");
+
+            Console.ReadKey();
+        }
+    }
+}
+
