faaf689812e17b5ee2a68858cc2c265937fa2ef6
unknown <emerson.oliverbr@gmail.com>
2025-06-23 22:59:15 -0300

    Add: DateTime e TimeSpan

---
+++ Topicos Especiais em CSharp/TipoTimeSpan/TipoTimeSpan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TipoTimeSpan;
+class Program
+{
+    static void Main(string[] args)
+    {
+        TimeSpan t1 = new TimeSpan();
+        TimeSpan t2 = new TimeSpan(0, 1, 30);
+        TimeSpan t3 = new TimeSpan(900000000L);
+        TimeSpan t4 = new TimeSpan(23, 06, 25);
+        TimeSpan t5 = new TimeSpan(1, 2, 11, 21);
+        TimeSpan t6 = new TimeSpan(1, 2, 11, 21, 321);
+
+        Console.WriteLine(t1);
+        Console.WriteLine(t2);
+        Console.WriteLine(t2.Ticks);
+        Console.WriteLine(t3);
+        Console.WriteLine(t4);
+        Console.WriteLine(t5);
+        Console.WriteLine(t6 + "\n");
+
+        TimeSpan t7 = TimeSpan.FromDays(1.5);
+        Console.WriteLine(t7);
+        TimeSpan t8 = TimeSpan.FromHours(1.5);
+        Console.WriteLine(t8);
+        TimeSpan t9 = TimeSpan.FromMinutes(1.5);
+        Console.WriteLine(t9);
+        TimeSpan t10 = TimeSpan.FromTicks(900000000L);
+        Console.WriteLine(t10);
+    }
+}
