31a9c4ae32560a1f7355cc55d9ce6534eb18196b
unknown <emerson.oliverbr@gmail.com>
2025-06-25 22:35:11 -0300

    Finalizando o modulo

---
+++ Topicos Especiais em CSharp/PadraoIso8601/PadraoIso8601/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PadraoIso8601;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+        DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z");
+
+        Console.WriteLine(d1);
+        Console.WriteLine(d2);
+
+        Console.ReadKey();
+    }
+}
