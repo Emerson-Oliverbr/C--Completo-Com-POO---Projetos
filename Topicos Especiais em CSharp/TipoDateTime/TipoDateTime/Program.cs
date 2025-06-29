d9c14aa1b5cc9b8ae654ae1806c70745d407c1b9
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-29 18:19:49 -0300

    Aprendendo mais sobre datas

---
+++ Topicos Especiais em CSharp/TipoDateTime/TipoDateTime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TipoDateTime;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime data1 = DateTime.Now;
+        Console.WriteLine(data1);
+
+        DateTime data2 = new DateTime(2025, 06, 18);
+        Console.WriteLine(data2);
+        DateTime data3 = new DateTime(2025, 06, 18, 23, 00, 00);
+        Console.WriteLine(data3);
+
+        DateTime data5 = DateTime.Today;
+        Console.WriteLine(data5);
+        DateTime data6 = DateTime.UtcNow;
+        Console.WriteLine(data6);
+
+        DateTime novaData = DateTime.Parse("1980-01-03");
+        Console.WriteLine(novaData);
+        DateTime novaData2 = DateTime.Parse("1980-01-03 13:05:58");
+        Console.WriteLine(novaData2);
+        DateTime novaData3 = DateTime.Parse("18/06/2025");
+        Console.WriteLine(novaData3);
+        DateTime novaData4 = DateTime.Parse("18/06/2025 23:12:10");
+        Console.WriteLine(novaData4);
+
+        Console.ReadKey();
+    }
+}
