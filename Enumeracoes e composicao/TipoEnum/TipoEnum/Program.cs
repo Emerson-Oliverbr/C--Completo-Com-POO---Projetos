371bbaf69c75ae39b67b85b52306c59edc504aed
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-29 21:55:43 -0300

    Aprendendo enumeracoes

---
+++ Enumeracoes e composicao/TipoEnum/TipoEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using TipoEnum.Entities;
+using TipoEnum.Entities.Enums;
+
+namespace TipoEnum;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order()
+        {
+            Id = 1080,
+            Moment = DateTime.Now,
+            Status = OrderStatus.PendingPayment
+        };
+
+        Console.WriteLine($"Status atual: {order}");
+
+        string txt = OrderStatus.PendingPayment.ToString();
+
+        OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        Console.WriteLine(os);
+        Console.WriteLine(txt);
+
+        Console.ReadKey();
+    }
+}
